25dbf98bc518a67e2671a39b63ffb6b5802f21b2
mavericksxx <130738587+mavericksxx@users.noreply.github.com>
2024-10-15 15:58:11 +0000

    fixed errors

--- lab-4/prog1.asm
+++ lab-4/prog1.asm
@@ -21,8 +21,9 @@ addi $s0, $s0, 4 # move by 4 locations
 addi $t0, $t0, 1 # increment loop
 
 # print int 
-li $a0, $t2
-syscall
+li $v0, 1           # Load syscall code for print integer
+move $a0, $t2       # Move the value to be printed into $a0
+syscall  
 j loop
 
 exit: 
