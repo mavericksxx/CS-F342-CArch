bc362f4723492e37138a933829ae1e4f3b5f7c25
mavericksxx <130738587+mavericksxx@users.noreply.github.com>
2024-10-30 05:57:01 +0000

    .

--- lab-6/prog1.asm
+++ lab-6/prog1.asm
@@ -4,36 +4,26 @@
 #    syscall
 #    la $a0, string1   // user input stored in string1
 
-.data                   
-prompt: .asciiz "Enter a string: "
-str: .space 100              
-msg:    .asciiz "\nThe length of the string is: "
 
-.text                   
+.data
+    string1: .asciiz "hello"
+    string2: .asciiz "the length of the string is: "
+
+.text
 main:
+    la $t0, string1
+    li $s0, 0                    # counter
+
+loop: 
+    add $t1, $t0, $s0            # adding t0 (base address of current char.) and s0 (counter) and storing in t1
+    lb $t2, 0($t0)               # read chars.   
+    beq $t2, $zero, exit         # once we reach the end of string (=0), exit out 
+    addi $s0, $s0, 1             # increment counter
+    j loop  
+
+exit: 
     li      $v0, 4           
-    la      $a0, prompt      
-    syscall                  
-
-    # read user input string
-    li      $v0, 8   
-    syscall        
-    la      $a0, str                                 
-
-    # calculate the length of the string
-    la      $t0, str         
-    li      $t1, 0           
-
-count:
-    lb      $t2, 0($t0)      
-    beq     $t2, 0, end
-    addi    $t1, $t1, 1      
-    addi    $t0, $t0, 1      
-    j       count       
-
-end:
-    li      $v0, 4           
-    la      $a0, msg         
+    la      $a0, string2         
     syscall                  
 
     li      $v0, 1           
---
+++ lab-6/prog2.asm
@@ -0,0 +1,3 @@
+# Reverse a string stored in memory. 
+# Store the reversed string in memory. 
+# Also display the reversed string to the user.
